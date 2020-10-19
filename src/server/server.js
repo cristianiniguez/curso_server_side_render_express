175e29c00d5bdc8abfb8d1541cb48fec38f4627c
cristianiniguez <felizpc1@gmail.com>
2020-10-19 17:08:55 -0400

    integracion de webpack con express

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
+/* eslint-disable global-require */
 import express from 'express';
 import dotenv from 'dotenv';
+import webpack from 'webpack';
 
 dotenv.config();
 
@@ -8,6 +10,16 @@ const app = express();
 
 if (ENV === 'development') {
   console.log('Development config');
+  const webpackConfig = require('../../webpack.config');
+  const webpacDevMiddleware = require('webpack-dev-middleware');
+  const webpacHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = {
+    port: PORT,
+    hot: true,
+  };
+  app.use(webpacDevMiddleware(compiler, serverConfig));
+  app.use(webpacHotMiddleware(compiler));
 }
 
 app.get('*', (req, res) => {
