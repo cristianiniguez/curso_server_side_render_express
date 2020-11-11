21ea484c30c25d106848cfd1eb5b4d19d5fe2c5e
cristianiniguez <felizpc1@gmail.com>
2020-11-10 20:28:16 -0400

    aplicar hashes al nombre de nuestros builds

--- src/server/server.js
+++ src/server/server.js
@@ -12,6 +12,7 @@ import { StaticRouter } from 'react-router-dom';
 import serverRoutes from '../frontend/routes/serverRoutes';
 import reducer from '../frontend/reducers';
 import initialState from '../frontend/initialState';
+import getManifest from './getManifest';
 
 dotenv.config();
 
@@ -31,20 +32,28 @@ if (ENV === 'development') {
   app.use(webpacDevMiddleware(compiler, serverConfig));
   app.use(webpacHotMiddleware(compiler));
 } else {
+  app.use((req, res, next) => {
+    if (!req.hashManifest) {
+      req.hashManifest = getManifest();
+      next();
+    }
+  });
   app.use(express.static(`${__dirname}/public`));
   app.use(helmet());
   app.use(helmet.permittedCrossDomainPolicies());
   app.disable('x-powered-by');
 }
 
-const setResponse = (html, preloadedState) => {
+const setResponse = (html, preloadedState, manifest) => {
+  const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
+  const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
   return `
     <!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="assets/app.css" type="text/css">
+        <link rel="stylesheet" href="${mainStyles}" type="text/css">
         <title>Platzi Videos</title>
       </head>
       <body>
@@ -52,7 +61,7 @@ const setResponse = (html, preloadedState) => {
         <script>
           window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
         </script>
-        <script src="assets/app.js" type="text/javascript"></script>
+        <script src="${mainBuild}" type="text/javascript"></script>
       </body>
     </html>
   `;
@@ -72,7 +81,7 @@ const renderApp = (req, res) => {
     'Content-Security-Policy',
     "default-src 'self'; img-src 'self' http://dummyimage.com https://gravatar.com; script-src 'self' 'sha256-n1jHrI2lN4ngk5Sy5hSo1W6k3uUvYHNkOoNvPbtTTo8='; style-src-elem 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; media-src 'self' https://mdstrm.com",
   );
-  res.send(setResponse(html, preloadedState));
+  res.send(setResponse(html, preloadedState, req.hashManifest));
 };
 
 app.get('*', renderApp);
