897dc9d7014c70e85fccf5cbbf8e805f4e3e55ba
cristianiniguez <felizpc1@gmail.com>
2020-11-11 16:32:33 -0400

    vendorfiles: generando el vendorfile

--- src/server/server.js
+++ src/server/server.js
@@ -47,6 +47,7 @@ if (ENV === 'development') {
 const setResponse = (html, preloadedState, manifest) => {
   const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
   const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
+  const vendorBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
   return `
     <!DOCTYPE html>
     <html lang="en">
@@ -61,6 +62,7 @@ const setResponse = (html, preloadedState, manifest) => {
         <script>
           window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
         </script>
+        <script src="${vendorBuild}" type="text/javascript"></script>
         <script src="${mainBuild}" type="text/javascript"></script>
       </body>
     </html>
